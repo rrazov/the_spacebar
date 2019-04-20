b2ba4a33e45a67f5fd77fd648454c5e1e4099547
rrazov <rokoraz20@gmail.com>
2019-04-20 11:15:54 +0200

    Updated controller and templates, added css,fonts,images and js directories

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,32 +4,44 @@ namespace App\Controller;
 
 
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name = "app_homepage")
      */
 
     public function homepage(){
 
-        return new Response("tako je mrki");
+        return $this->render('/article/homepage.html.twig');
 
     }
 
     /**
-     * @Route("/news/{id}")
+     * @Route("/news/{id}", name="article_show")
      */
     public function show($id){
 
         $comments = ['ovo je 1. kom','ovo je 2. kom','ovo je 3. kom'];
+
         return $this->render('/article/show.html.twig', [
                 'title' => 'ovo je title',
+                'id' => $id,
                 'comments' => $comments
             ]);
 
+    }
+
+    /**
+     * @Route("/news/{id}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($id, LoggerInterface $logger)
+    {
+        $logger->info('Article is being hearted');
+        return $this->json(['hearts' => rand(5, 100)]);
     }
 }
