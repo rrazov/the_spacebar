9ec8f62dd70037e21d09b39ed0a4d36e0b2c132f
rrazov <rokoraz20@gmail.com>
2019-05-03 16:09:33 +0200

    add user login form

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -16,33 +16,44 @@ use Faker\Generator;
 
 abstract class BaseFixture extends Fixture
 {
-    /**
-     * @var ObjectManager
-     */
+    /** @var ObjectManager */
     private $manager;
-
-    /**
-     * @var Generator
-     */
+    /** @var Generator */
     protected $faker;
-
+    private $referencesIndex = [];
+    abstract protected function loadData(ObjectManager $manager);
     public function load(ObjectManager $manager)
     {
         $this->manager = $manager;
         $this->faker = Factory::create();
         $this->loadData($manager);
     }
-
-    abstract protected function loadData(ObjectManager $manager);
-
-    protected function createMany(string $className, int $count, callable $factory)
+    /**
+     * Create many objects at once:
+     *
+     *      $this->createMany(10, function(int $i) {
+     *          $user = new User();
+     *          $user->setFirstName('Ryan');
+     *
+     *           return $user;
+     *      });
+     *
+     * @param int      $count
+     * @param string   $groupName Tag these created objects with this group name,
+     *                            and use this later with getRandomReference(s)
+     *                            to fetch only from this specific group.
+     * @param callable $factory
+     */
+    protected function createMany(int $count, string $groupName, callable $factory)
     {
         for ($i = 0; $i < $count; $i++) {
-            $entity = new $className();
-            $factory($entity, $i);
+            $entity = $factory($i);
+            if (null === $entity) {
+                throw new \LogicException('Did you forget to return the entity object from your callback to BaseFixture::createMany()?');
+            }
             $this->manager->persist($entity);
-            // store for usage later as App\Entity\ClassName_#COUNT#
-            $this->addReference($className . '_' . $i, $entity);
+            // store for usage later as groupName_#COUNT#
+            $this->addReference(sprintf('%s_%d', $groupName, $i), $entity);
         }
     }
 }
